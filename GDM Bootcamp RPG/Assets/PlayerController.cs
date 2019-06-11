e48c1a93c3600b22ce6da138e34a112d66cc43f1
Brendan <bshizzle1234@gmail.com>
2019-06-11 08:29:49 -0700

    Added playermotor for simple player movement

--- GDM Bootcamp RPG/Assets/PlayerController.cs
+++ GDM Bootcamp RPG/Assets/PlayerController.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
     public LayerMask movementMask;
 
     Camera cam;
+    PlayerMotor motor;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
     }
 
     // Update is called once per frame
@@ -26,9 +29,7 @@ public class PlayerController : MonoBehaviour
             // TODO: May need to explain what the "out" keyword does. Passing by value / ref
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
-                Debug.Log("We hit " + hit.collider.name + " " + hit.point); 
-                // Move our player to what we hit
-
+                motor.MoveToPoint(hit.point);
                 // Stop focusing any objects
             }
         }
