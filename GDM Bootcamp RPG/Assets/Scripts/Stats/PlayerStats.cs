f213d5cd751df812b58430826e204ba15fb36fdb
Brendan <bshizzle1234@gmail.com>
2019-07-19 18:30:40 -0700

    Intermediate commit. Added player stats script

---
+++ GDM Bootcamp RPG/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChange += OnEquipmentChanged;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            armor.AddModifier(newItem.damageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            armor.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+}
